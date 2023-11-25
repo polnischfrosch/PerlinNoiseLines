71b709209e5672700b31458769e637fe2425afed
polnischfrosch <wagner.aaron.1997@gmail.com>
2023-11-25 22:49:03 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import "./style.css";
+import * as THREE from 'three';
 import { Scene, OrthographicCamera, WebGLRenderer, Color, Vector2 } from "three";
 import { Line2, LineGeometry, LineMaterial } from "three-fatline";
-import { AccumProgram } from "./accum";
-import { createNoise2D } from "simplex-noise";
+import { createNoise3D } from "simplex-noise";
 
-const noise2D = createNoise2D();
+const noise3D = createNoise3D();
+const clock = new THREE.Clock();
+var myLines = [];
+var myLine = null;
+var materialArray = [];
+var speed = 0.2;
 
 const scene = new Scene();
 const res = 800;
@@ -25,56 +30,52 @@ renderer.setSize(res, res);
 document.body.appendChild(renderer.domElement);
 
 let hasRedLine = false;
-createLines();
 
-scene.background = new Color("rgb(34, 30, 27)").convertLinearToSRGB();
+// Fill material Array
+for (let r = -20; r < 20; r++){
+  if (r==0){
+  materialArray.push(
+  new LineMaterial({
+    color: "rgb(255, 70, 70)", // Red color
+    linewidth: 3, // Thickestline
+    resolution: new Vector2(res, res),
+    dashed: false,
+    dashScale: 1,
+    dashSize: 1,
+    gapSize: 1,
+    })
+  )
+  }
+  materialArray.push(createRandomDashedMaterial());
+}
 
-const accumProgram = new AccumProgram(renderer);
-(function animate() {
-  requestAnimationFrame(animate);
+scene.background = new Color("rgb(34, 30, 27)");
 
-  accumProgram.accumulate(() => {
-    camera.position.set(Math.random(), Math.random(), 0);
+
+function animate() {
+    requestAnimationFrame(animate);
+    removeLines();
+    createLines();
     renderer.render(scene, camera);
-  });
-})();
+
+}
 
 function createLines() {
+  const elapsedTime = clock.getElapsedTime();
+  var index = 0
   for (let r = -20; r < 20; r++) {
     let vertices = [];
 
-    let wnoise = noise2D(0, r * 0.125) * 1.0;
+    let wnoise = noise3D(0, r * 0.125,0.01) * 1.0;
     let lwidth = 0.25 + Math.pow(wnoise * 0.5 + 1, 2);
-    let lcolor = "rgb(241, 231, 222)";
-
-    if (lwidth > 1.5 && !hasRedLine && Math.abs(r) < 4) {
-      hasRedLine = true;
-      lcolor = "rgb(255, 150, 140)";
-    } 
-
-    let dashed = Math.random() > 0.5;
-    let dashScale = 1;
-    let dashSize = Math.pow(Math.random(), 2) * 15 + 4;
-    let gapSize = dashSize * (0.5 + Math.random() * 1);
-
-    const material = new LineMaterial({
-      color: lcolor,
-      linewidth: lwidth,
-      resolution: new Vector2(res, res), // resolution of the viewport
-      // dashed, dashScale, dashSize, gapSize
-      dashed,
-      dashScale,
-      dashSize,
-      gapSize,
-    });
 
     for (let i = 0; i < 100; i++) {
       let height = 0;
-      height += noise2D(i * 0.0189 * 1, r * 0.125) * 2.0;
-      height += noise2D(i * 0.0189 * 2, r * 0.125) * 1.0;
-      height += noise2D(i * 0.0189 * 4, r * 0.125) * 0.5;
-      height += noise2D(i * 0.0189 * 8, r * 0.125) * 0.25;
-      height += noise2D(i * 0.0189 * 16, r * 0.125) * 0.125;
+      height += noise3D(i * 0.0189 * 1, r * 0.125,0+elapsedTime*speed) * 2.0;
+      height += noise3D(i * 0.0189 * 2, r * 0.125,0+elapsedTime*speed) * 1.0;
+      height += noise3D(i * 0.0189 * 4, r * 0.125,0+elapsedTime*speed) * 0.5;
+      height += noise3D(i * 0.0189 * 8, r * 0.125,0+elapsedTime*speed) * 0.25;
+      height += noise3D(i * 0.0189 * 16, r * 0.125,0+elapsedTime*speed) * 0.125;
       
       vertices.push(
         -330 + 660 * (i / 100), 
@@ -86,9 +87,52 @@ function createLines() {
     const geometry = new LineGeometry();
     geometry.setPositions(vertices);
 
-    const myLine = new Line2(geometry, material);
+    const myLine = new Line2(geometry, materialArray[index]);
+    myLines.push(myLine);
     myLine.computeLineDistances();
 
     scene.add(myLine);
+    index ++;
   }
 }
+
+function removeLines() {
+  while (myLines.length > 0) {
+      let line = myLines.pop();
+
+      // Remove the line from the scene
+      scene.remove(line);
+
+      // Dispose of geometry and material
+      if (line.geometry) {
+          line.geometry.dispose();
+      }
+
+      if (line.material) {
+          line.material.dispose();
+      }
+  }
+  myLines = [];
+  myLine = null;
+  hasRedLine = false;
+}
+
+// Function to create a new material with random dashed properties
+function createRandomDashedMaterial() {
+  let dashed = Math.random() > 0.5;
+  let dashScale = 1;
+  let dashSize = Math.pow(Math.random(), 2) * 15 + 4;
+  let gapSize = dashSize * (0.5 + Math.random() * 1);
+
+  return new LineMaterial({
+      color: "rgb(205,187,170)", // Default color, can be changed later
+      linewidth: 0.2 + Math.random()*3, // Default linewidth, can be changed later
+      resolution: new Vector2(res, res),
+      dashed: dashed,
+      dashScale: dashScale,
+      dashSize: dashSize,
+      gapSize: gapSize,
+  });
+}
+
+animate()
